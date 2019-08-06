6084567289c21bdf68800ab97d4e055bf834e0c3
Dimitre <dimitre@users.noreply.github.com>
2019-08-06 14:05:05 -0300

    up

--- MicroUI/src/ofApp.cpp
+++ MicroUI/src/ofApp.cpp
@@ -10,11 +10,21 @@ void ofApp::update(){
 
 void ofApp::draw(){
 	ofBackground(40);
-	ofSetColor(m.pFloat["r"], m.pFloat["g"], m.pFloat["b"]);
-	ofDrawCircle(m.pFloat["x1"],m.pFloat["y1"],m.pFloat["radius1"]);
+	for (int a=1; a<4; a++) {
+		ofSetColor(m.pFloat["r" + ofToString(a)],
+				   m.pFloat["g" + ofToString(a)],
+				   m.pFloat["b" + ofToString(a)]);
+		ofDrawCircle(
+					 m.pFloat["x" + ofToString(a)],
+					 m.pFloat["y" + ofToString(a)],
+					 m.pFloat["radius" + ofToString(a)]);
+	}
 	
-	ofSetColor(m.pFloat["r2"], m.pFloat["g2"], m.pFloat["b2"]);
-	ofDrawCircle(m.pFloat["x2"],m.pFloat["y2"],m.pFloat["radius2"]);
+//	ofSetColor(m.pFloat["r"], m.pFloat["g"], m.pFloat["b"]);
+//	ofDrawCircle(m.pFloat["x1"],m.pFloat["y1"],m.pFloat["radius1"]);
+//
+//	ofSetColor(m.pFloat["r2"], m.pFloat["g2"], m.pFloat["b2"]);
+//	ofDrawCircle(m.pFloat["x2"],m.pFloat["y2"],m.pFloat["radius2"]);
 }
 
 void ofApp::keyPressed(int key){
