6a7a58dc9a0e18c8f3c5ed660dce5127ff25ebd8
Dimitre <dimitre@users.noreply.github.com>
2019-08-08 13:07:50 -0300

    updates

--- MiawTest/src/ofApp.cpp
+++ MiawTest/src/ofApp.cpp
@@ -58,9 +58,9 @@ void ofApp::draw(){
 	
 	//ofSetColor(m.pFloat["r"], m.pFloat["g"], m.pFloat["b"]);
 	
-	//ofSetColor(m.pVec3["color"].x * 255.0, m.pVec3["color"].y * 255.0, m.pVec3["color"].z * 255.0);
-	glm::vec3 cor = m.pVec3["color"] * 255.0;
-	ofSetColor((ofColor)cor);
+	ofSetColor(m.pVec3["color"].x * 255.0, m.pVec3["color"].y * 255.0, m.pVec3["color"].z * 255.0);
+	//glm::vec3 cor = m.pVec3["color"] * 255.0;
+	//ofSetColor((ofColor)cor);
 //	ofEnableBlendMode(OF_BLENDMODE_SCREEN);
 //ac	ofEnableBlendMode(OF_BLENDMODE_MULTIPLY); //ADD nao, screen tvz
 	
@@ -79,8 +79,12 @@ void ofApp::draw(){
 		float y = map(a,0,100,0,100,m.pFloat["shaper"]) * mult;
 		ofDrawCircle(x,y,3);
 	}
+	
+	
+	
+	
 	fbo.end();
-	fbo.draw(300,10);
+	fbo.draw(600,10);
 	
 
 }
