52c8a7d681b3964544bf7ada468eeb8b75f1fd3c
Dimitre <dimitre@users.noreply.github.com>
2019-08-10 14:21:14 -0300

    updates

--- src/ofxMicroUI.h
+++ src/ofxMicroUI.h
@@ -5,11 +5,11 @@ having an ofFbo for each element to redraw and store, only use texture when not 
 three boolean controls, toggle, hold and bang.
 bind event to elements.
 event system.
+ 
+ofParameter?
 
 make elementgroup to act as one. in xml, draw, etc.
 */
-
-
 
 
 class ofxMicroUI : public ofBaseApp {
@@ -48,275 +48,17 @@ public:
 	} settings;
 	
 	
-	// default element skeleton
-	class element {
-	public:
-		microUISettings * _settings = NULL;
-		
-		string name = "";
-		ofRectangle rect = ofRectangle(0,0,240,20);
-		ofRectangle rectVal = ofRectangle(0,0,240,20);
-		string labelText = "";
 
-		bool wasPressed = false;
-		
-		virtual void set(float v) {}
-		virtual void set(int v) {}
-		virtual void set(bool v) {}
-		virtual void set(string v) {}
-		virtual void set(glm::vec2 v) {}
-		virtual void set(glm::vec3 v) {}
-
-		glm::vec2 labelPos = glm::vec2(5, 16);
-
-		virtual void drawLabel() {
-			ofSetColor(255);
-			ofDrawBitmapString(labelText, rect.x + labelPos.x, rect.y + labelPos.y);
-		}
-		
-		virtual void drawElement() {
-		}
-		
-		virtual void draw() {
-			drawElement();
-			drawLabel();
-		}
-		
-		virtual void setValFromMouse(int x, int y) {
-		}
-		
-		virtual void checkMouse(int x, int y, bool first = false) {
-			if (rect.inside(x, y)) {
-				wasPressed = true;
-				setValFromMouse(x,y);
-			} else {
-				if (wasPressed) {
-					wasPressed = false;
-					setValFromMouse(x,y);
-				}
-			}
-		}
-		
-		virtual void mouseRelease(int x, int y) {
-			if (rect.inside(x, y)) {
-				wasPressed = false;
-			}
-		}
-		
-		element() {}
-		~element() {}
-
-		void setupElement(string & n, microUISettings & s) {
-			_settings = &s;
-			rect.position = ofPoint(_settings->xy);
-			name = n;
-			labelText = n;
-			_settings->advanceLine();
-		}
-	};
+#include "ofxMicroUIElements.h"
 	
 	
-	class label : public element {
-	public:
-		label(string & n, microUISettings & s) {
-			setupElement(n, s);
-		}
-	};
-	
-	
-	// new prototype
-	class group : public element {
-	public:
-		vector <element *> elements;
-		glm::vec3 * _val = NULL;
-		
-		group() {}
-		group(string & n, microUISettings & s, glm::vec3 & v) {
-			_val = &v;
-			setupElement(n, s);
-		}
-		
-		void checkMouse(int x, int y, bool first = false) {
-			if (rect.inside(x, y)) {
-				for (auto & e : elements) {
-					e->checkMouse(x, y, first);
-				}
-			}
-		}
-		
-		void mouseRelease(int x, int y) {
-			if (rect.inside(x, y))
-			{
-				for (auto & e : elements) {
-					e->mouseRelease(x,y);
-				}
-				wasPressed = false;
-			}
-		}
-
-		void draw() {
-			for (auto & e : elements) {
-				e->draw();
-			}
-		}
-	};
-	
-	class vec3 : public group {
-	public:
-		vec3(string & n, microUISettings & s, glm::vec3 & v) {
-			_val = &v;
-			setupElement(n, s);
-			glm::vec3 vals = glm::vec3(0,1,.5);
-			//friend class?
-			//string name = "GROUP";
-			string x = "x";
-			string y = "y";
-			string z = "z";
-			elements.push_back(new label(name, s));
-			elements.push_back(new slider(x, s, vals, _val->x));
-			elements.push_back(new slider(y, s, vals, _val->y));
-			elements.push_back(new slider(z, s, vals, _val->z));
-
-			rect = elements[0]->rect;
-			for (auto & e : elements) {
-				rect.growToInclude(e->rect);
-			}
-		}
-		
-		void set(glm::vec3 v) {
-			*_val = v;
-			for (auto & e : elements) {
-				if (e->name == "x") {
-					e->set(_val->x);
-				}
-				if (e->name == "y") {
-					e->set(_val->y);
-				}
-				if (e->name == "z") {
-					e->set(_val->z);
-				}
-			}
-		}
-		
-		void set(string s) {
-			vector<string> vals = ofSplitString(s, ", ");
-			set(glm::vec3(ofToFloat(vals[0]), ofToFloat(vals[1]), ofToFloat(vals[2])));
-		}
-
-		glm::vec3 getVal() {
-			return *_val;
-		}
-	};
-
-
-	class slider : public element {
-	public:
-		float * _val = NULL;
-		float min = 0;
-		float max = 1;
-		//		float val = 0.5;
-
-//		void drawLabel() {
-//			name + " " + ofToString(val);
-//		}
-		
-		void drawElement() {
-			ofSetColor(127);
-			ofDrawRectangle(rect);
-			ofSetColor(80);
-			ofDrawRectangle(rectVal);
-		}
-		
-		float getVal() {
-			return *_val;
-		}
-		
-		void set(float v) {
-			//val = v;
-			if (_val != NULL) {
-				*_val = v;
-			}
-			rectVal.width = ofMap(v, min, max, 0, rect.width);
-			labelText = name + " " + ofToString(*_val);
-		}
-		
-		void setValFromMouse(int x, int y) {
-			int xx = ofClamp(x, rect.x, rect.x + rect.width);
-			int yy = ofClamp(y, rect.y, rect.y + rect.height);
-			ofPoint xy = ofPoint (xx,yy) - ofPoint(rect.x, rect.y);
-			ofPoint wh = ofPoint (rect.width, rect.height);
-			ofPoint val = min + (max-min)*(xy/wh);
-			set(val.x);
-		}
-		
-		
-		slider(string & n, microUISettings & s, glm::vec3 val, float & v) { // : name(n)
-			setupElement(n, s);
-			_val = &v;
-			rectVal = rect;
-			min = val.x;
-			max = val.y;
-			set(val.z);
-		}		
-	};
-	
-	class booleano : public element {
-	public:
-		bool * _val = NULL;
-		
-		bool getVal() {
-			return *_val;
-		}
-		
-		void set(bool v) {
-			*_val = v;
-		}
-		
-		void toggle() {
-			set(*_val ^ 1);
-		}
-		
-		void setValFromMouse(int x, int y) {
-			if (wasPressed) {
-				toggle();
-			}
-		}
-		
-		void checkMouse(int x, int y, bool first = false) {
-			if (rect.inside(x, y)) {
-				if (!wasPressed) {
-					toggle();
-				}
-				wasPressed = true;
-			} else {
-				wasPressed = false;
-			}
-		}
-		
-		
-		void drawElement() {
-			ofSetColor(127);
-			ofDrawRectangle(rect.x, rect.y, 20, 20);
-			
-			if (*_val) {
-				ofSetColor(40);
-				ofDrawRectangle(rectVal);
-			}
-		}
-		
-		booleano (string & n, microUISettings & s, bool val, bool & v) {
-			setupElement(n, s);
-			rectVal.position = rect.position + ofPoint(5,5);
-			rectVal.width = rectVal.height = 10;
-			_val = &v;
-			set(val);
-			labelPos = glm::vec2(25, 16);
-		}
-
-	};
-
 	// TODO
-	element * getElement(string n) {
+	//
+	element * getElement(string & n) {
+	}
+	
+	// and other kinds
+	slider * getSlider(string & n) {
 	}
 	
 	vector <element*> elements;
@@ -380,46 +122,6 @@ public:
 		alert("destroy");
 	}
 	
-
-	void createFromText(string fileName) {
-		alert("createFromText " + fileName);
-		vector <string> lines = textToVector(fileName);
-		for (auto & l : lines) {
-
-			vector <string> cols = ofSplitString(l, "\t");
-			if (cols.size() == 1) {
-				if (l == "") {
-					settings.advanceLine();
-				}
-				else if (l == "newCol") {
-					settings.newCol();
-				}
-			}
-			if (cols.size() >= 2) {
-				string name = cols[1];
-
-				if (cols[0] == "label") {
-					elements.push_back(new label(name, settings));
-				}
-				
-				else if (cols[0] == "vec3") {
-					elements.push_back(new vec3(name, settings, pVec3[name]));
-				}
-
-				else if (cols[0] == "float") {
-					vector <string> values = ofSplitString(cols[2]," ");
-					glm::vec3 vals = glm::vec3(ofToFloat(values[0]),ofToFloat(values[1]),ofToFloat(values[2]));
-					elements.push_back(new slider(name, settings, vals, pFloat[name]));
-				}
-
-				else if (cols[0] == "bool") {
-					bool val = ofToBool(cols[2]);
-					pBool[name] = val;
-					elements.push_back(new booleano (name, settings, val, pBool[name]));
-				}
-			}
-		}
-	}
 	
 	void load(string xml) {
 		alert("load " + xml);
@@ -482,5 +184,47 @@ public:
 			//floats.appendChild(e->name).set(((slider*)e)->getVal());
 		}
 		xmlSettings.save(xml);
+	}
+	
+	
+	
+	void createFromText(string fileName) {
+		alert("createFromText " + fileName);
+		vector <string> lines = textToVector(fileName);
+		for (auto & l : lines) {
+			
+			vector <string> cols = ofSplitString(l, "\t");
+			if (cols.size() == 1) {
+				if (l == "") {
+					settings.advanceLine();
+				}
+				else if (l == "newCol") {
+					settings.newCol();
+				}
+			}
+			if (cols.size() >= 2) {
+				string name = cols[1];
+				
+				if (cols[0] == "label") {
+					elements.push_back(new label(name, settings));
+				}
+				
+				else if (cols[0] == "vec3") {
+					elements.push_back(new vec3(name, settings, pVec3[name]));
+				}
+				
+				else if (cols[0] == "float") {
+					vector <string> values = ofSplitString(cols[2]," ");
+					glm::vec3 vals = glm::vec3(ofToFloat(values[0]),ofToFloat(values[1]),ofToFloat(values[2]));
+					elements.push_back(new slider(name, settings, vals, pFloat[name]));
+				}
+				
+				else if (cols[0] == "bool") {
+					bool val = ofToBool(cols[2]);
+					pBool[name] = val;
+					elements.push_back(new toggle (name, settings, val, pBool[name]));
+				}
+			}
+		}
 	}
 };
