c6c1087b65599c29a65d6c6fde9a7bf09292e11a
Dimitre <dimitre@users.noreply.github.com>
2019-08-13 13:08:49 -0300

    radio fix

--- src/ofxMicroUI.h
+++ src/ofxMicroUI.h
@@ -19,6 +19,11 @@ public:
 	map <string, glm::vec3>	pVec3;
 
 	struct microUISettings {
+	private:
+		bool flowVert = true;
+		
+	public:
+
 		/*
 		 Settings control the flow (distribution on xy) of elements, settings of elements, margins, etc.
 		 a pointer is added to each element so they all obey to the same settings.
@@ -28,20 +33,39 @@ public:
 		 slider dimensions.
 		 */
 		glm::vec2 xy = glm::vec2(10,10);
+		float xBak = 0;
 		map <string, int> pInt;
 		int margin = 10;
 		int spacing = 4;
 		ofRectangle elementRect = ofRectangle(0,0,240,20);
 		
-		bool flowVert = true;
 		ofRectangle flowRect;
+		
+		void setFlowVert(bool s) {
+			// if flow was horizontal and we change to horizontal, save the x coordinate
+			if (flowVert && !s) {
+				xBak = xy.x;
+				//cout << "backup of the x coordinate " << xBak << endl;
+			}
+			// if flow was vertical and we change to vertical, bring back the backup x coordinate.
+			if (!flowVert && s) {
+				xy.x = xBak;
+				//cout << "restore of the x coordinate " << xy.x <<  endl;
+			}
+			flowVert = s;
+		}
 
 		void advanceLine() {
 			if (flowVert) {
 				xy.y += 20 + spacing;
 			} else {
-				//cout << flowRect.width << endl;
-				xy.x += flowRect.width + spacing;
+				int newX = xy.x + flowRect.width - xBak;
+				if (newX > 240 ) {
+					xy.y += 20 + spacing;
+					xy.x = xBak;
+				} else {
+					xy.x += flowRect.width + spacing;
+				}
 			}
 		}
 		
