abfa1e1a999fd6bd4a4e81905a169020ed660658
Dimitre <dimitre@users.noreply.github.com>
2021-03-30 13:42:03 -0300

    updates

--- src/ofxMicroUIRemote.h
+++ src/ofxMicroUIRemote.h
@@ -1,269 +1,255 @@
 #pragma once
-
-// comentei events.
-// #include "ofEvents.h"
 #include "ofxMicroUI.h"
 #include "ofxOsc.h"
-
 #if defined( TARGET_OF_IPHONE ) || defined( TARGET_OF_IOS ) || defined( TARGET_ANDROID )
 #define MICROUI_TARGET_TOUCH
 #endif
-
 #ifdef MICROUI_TARGET_TOUCH
 #include "ofxAccelerometer.h"
 #endif
-
-
-
-class ofxMicroUIRemote2 : public ofBaseApp {
-public:
-    string name = "";
-    enum bundleUsage {
-        USEBUNDLE_NO,
-        USEBUNDLE_YES,
-        USEBUNDLE_ALL,
-    };
-    bundleUsage useBundle = USEBUNDLE_YES;
-
-    ofxMicroUI::inspector * oscInfo = NULL;
-    ofxMicroUI::inspector * oscInfoReceive = NULL;
-    ofxMicroUI::inspector * oscIP = NULL;
-};
-
+/*================================================================
+remote	192.168.1.240:9000
+local	8000
+useBundle	no
+#useBundle	yes
+#useBundle	all
+addUIByTag	unreal,unreal_level,unreal_color,unreal_region
+*/
 class ofxMicroUIRemote : public ofBaseApp {
 public:
-
-    string name = "";
-	enum bundleUsage {
-		USEBUNDLE_NO,
-		USEBUNDLE_YES,
-		USEBUNDLE_ALL,
-	};
-	bundleUsage useBundle = USEBUNDLE_YES;
-	
-//	bool useBundle = true;
-	
-	// change to ofxMicroUI Alerts
-
-	// novidade 2021.
-	ofxMicroUI * _uiMaster = NULL;
-
-	// no caso de interface remota o pointer _ui aponta 
-	// pra u que é ela propria.
-	ofxMicroUI u;
-	ofxMicroUI * _ui = &u;
+	// UIMASTER, de onde saem todas as outras. talvez trazer o soft aqui?
+	ofxMicroUI * _u = NULL;
+	string name = "";
 	string configFile = "";
-
-	ofxMicroUI::inspector * oscInfo = NULL;
-	ofxMicroUI::inspector * oscInfoReceive = NULL;
-    ofxMicroUI::inspector * oscIP = NULL;
-    
-
-	bool sendOnLoadPreset = true;
 
 	ofxOscSender 	send;
 	ofxOscReceiver 	receive;
+	bool useSend = false;
+	bool useReceive = false;
 
-	string 	serverAddress = "";
-	int 	serverPort = 9000;
-	string 	remoteAddress = "";
-	int 	remotePort = 8000;
-	
 	ofxOscBundle bundle;
-	
-	ofEvent<string> eventMessage;
 	map <string, ofxMicroUI *> _nameUIs;
-	
-	map <string, std::function<void()>> msgFunction;
-	
-	string reservedAddress = "/reservedAddress/";
-	
-	bool isServer = true;
-	
-	void uiEventMaster(string & s) {
-//		cout << "SETUP REMOTE " << s << endl;
-		if (s == "setup") {
-			if (configFile != "") {
-				loadConfig(configFile);
-				
-				if (isServer) {
-					setupServer();
-				}
-				else {
-					setupRemote();
-				}
-			}
-		}
-	}
-	
-	void loadConfig(string file) {
-//		 cout << "loadConfig " << file << endl;
-		if (ofFile::doesFileExist(file)) {
-			map <string, string> configs = ofxMicroUI::loadConfigPairs(file);
-			if (configs.count("remotePort")) {
-				remotePort = ofToInt(configs["remotePort"]);
-			}
-			
-			if (configs.count("serverPort")) {
-				serverPort = ofToInt(configs["serverPort"]);
-			}
-			
-			if (configs.count("remoteAddress")) {
-				remoteAddress = configs["remoteAddress"];
-			}
-			
-			if (configs.count("serverAddress")) {
-				serverAddress = configs["serverAddress"];
-			}
-			
-			if (configs.count("useBundle")) {
-				if (configs["useBundle"] == "no") {
-					useBundle = USEBUNDLE_NO;
-				}
-				else if (configs["useBundle"] == "yes") {
-					useBundle = USEBUNDLE_YES;
-				}
-				else if (configs["useBundle"] == "all") {
-					useBundle = USEBUNDLE_ALL;
-				}
-//				serverAddress = configs["serverAddress"];
-			}
-			
-			if (configs.count("server")) {
-				isServer = true;
-			}
-			
-			if (configs.count("remote")) {
-				isServer = false;
-			}
-			
-			if (configs.count("addUIByTag")) {
-				addUIByTag(configs["addUIByTag"]);
-			}
-			
-            if (configs.count("addAllUIs")) {
-                addAllUIs();
-            }
-			
-            if (configs["addUIByNames"] != "") {
-                addUIByNames(configs["addUIByNames"]);
-            }
-		}
-	}
-	
+    
+    // apenas se for o empty remote
+    ofxMicroUI u;
+    
+    bool sendOnLoadPreset = true;
+
+
 	void setup() {
-		ofAddListener(_uiMaster->uiEventMaster, this, &ofxMicroUIRemote::uiEventMaster);
-		// ofAddListener(ofEvents().draw, this, &ofxMicroUIRemote::onDraw);
+		ofAddListener(_u->uiEventMaster, this, &ofxMicroUIRemote::uiEventMaster);
 		ofAddListener(ofEvents().update, this, &ofxMicroUIRemote::onUpdate);
 	}
 	
-	ofxMicroUIRemote(ofxMicroUI * _ui, string f) : _uiMaster(_ui), configFile(f) {
-//		loadConfig(f);
-		setup();
-	}
-
-	ofxMicroUIRemote(string f) : configFile(f) {
-//		loadConfig(f);
+	ofxMicroUIRemote(ofxMicroUI * _ui, string n, string f) : _u(_ui), name(n), configFile(f) {
 		setup();
 	}
 
 	ofxMicroUIRemote() {
 		setup();
 	}
+
+	enum bundleUsage {
+		USEBUNDLE_NO,
+		USEBUNDLE_YES,
+		USEBUNDLE_ALL,
+	};
+	
+	bundleUsage useBundle = USEBUNDLE_YES;
+
+	ofxMicroUI::inspector * oscInfo = NULL;
+	ofxMicroUI::inspector * oscInfoReceive = NULL;
+	ofxMicroUI::inspector * oscIP = NULL;
+
+	void uiEventMaster(string & s) {
+		if (s == "setup") {
+			string message = "";
+            message += "ofxMicroUIRemote " + name + "\r";
+
+			for (auto & l : ofxMicroUI::textToVector(configFile)) {
+				vector <string> cols = ofSplitString(l, "\t");
+				string name = cols[0];
+
+				if (name == "remote") {
+					vector <string> vals = ofSplitString(cols[1], ":");
+					try {
+						useSend = send.setup(vals[0], ofToInt(vals[1]));
+					} catch (exception){
+						message += "ofxMicroUIRemote setupServer :: ||| NO INTERNET |||";
+					}
+
+					if (useSend) {
+                        message += string("sending to remote ") + vals[0] + ":" + vals[1] + "\r";
+					}
+				}
+				else if (name == "local") {
+					try {
+						receive.setup(ofToInt(cols[1]));
+						message += "receiving in local port " + cols[1];
+					} catch (const exception){
+						cout << "ofxMicroUIRemote setupRemote :: ||| NO INTERNET |||" << endl;
+					}
+				}
+
+				else if (name == "useBundle") {
+					if (cols[1] == "no") {
+						useBundle = USEBUNDLE_NO;
+					}
+					else if (cols[1] == "yes") {
+						useBundle = USEBUNDLE_YES;
+					}
+					else if (cols[1] == "all") {
+						useBundle = USEBUNDLE_ALL;
+					}
+				}
+
+				else if (name == "addUIByTag") {
+					addUIByTag(cols[1]);
+				}
+				else if (name == "addAllUIs") {
+					addAllUIs();
+				}
+				else if (name == "addUIByNames") {
+					addUIByNames(cols[1]);
+				}
+			}
+			ofxMicroUI::messageBox(message);
+		}
+	}
+
+
 	
 	~ofxMicroUIRemote() {}
-	
 
 	void addUI(ofxMicroUI * ui) {
-//		cout << "addUI " << ui->uiName << endl;
 		_nameUIs[ui->uiName] = ui;
 		ofAddListener(_nameUIs[ui->uiName]->uiEvent, this, &ofxMicroUIRemote::uiEvent);
-		ofAddListener(_nameUIs[ui->uiName]->uiEventMaster, this, &ofxMicroUIRemote::uiEventString);
+		// ofAddListener(_nameUIs[ui->uiName]->uiEventMaster, this, &ofxMicroUIRemote::uiEventString);
 	}
-	// fazer um addAllUis
 	
 	void addAllUIs() {
-		for (auto & u : _uiMaster->allUIs) {
-            // cout << "|||| ADD " << u->uiName << endl;
+		for (auto & u : _u->allUIs) {
 			addUI(u);
 		}
 	}
 
 	void addUIByTag(string tags) {
-        for (auto tag : ofSplitString(tags, ",")) {
-            cout << "ADDUIBYTAG " << tag << endl;
-            for (auto & u : _uiMaster->allUIs) {
-                if (u->uiTag == tag) {
-                    cout << u->uiName <<endl;
-                    addUI(u);
-                }
-            }
-        }
-	}
-    
-    void addUIByNames(string s) {
-        vector <string> names = ofSplitString(s, ",");
-        for (auto & n : names) {
-            addUI(&_uiMaster->uis[n]);
-        }
-    }
-	
-    void setupAll() {
-        if (oscIP != NULL) {
-            string ip = name + " remote:" + remoteAddress + ":" + ofToString(remotePort);
-            oscIP->set(ip);
-        }
-    }
-    
-	void setupServer() {
-		receive.setup(serverPort);
-		bool serverIsSetup;
-		
-		try {
-			serverIsSetup = send.setup(remoteAddress, remotePort);
-//			cout << "trying " << remoteAddress << ":" << remotePort << endl;
-		} catch (exception){
-			cout << "ofxMicroUIRemote :: &&& no internet &&&" << endl;
+		for (auto tag : ofSplitString(tags, ",")) {
+			cout << "ADDUIBYTAG " << tag << endl;
+			for (auto & u : _u->allUIs) {
+				if (u->uiTag == tag) {
+					cout << u->uiName <<endl;
+					addUI(u);
+				}
+			}
 		}
-		if (serverIsSetup) {
-			string message = "ofxMicroUIRemote server \r" 
-			+ string("server = ") + serverAddress + ":" + ofToString(serverPort) + "\r"
-			+ string("remote = ") + remoteAddress + ":" + ofToString(remotePort) ; //+ "\r"
-//			 cout << message << endl;
-			ofxMicroUI::messageBox(message);
-		}
-        
-        setupAll();
 	}
 	
-	void setupRemote() {
-		try {
-			receive.setup(remotePort);
-			string message = "ofxMicroUIRemote REMOTE \r";
-			+ "Port = " + ofToString(remotePort);
-			ofxMicroUI::messageBox(message);
-		} catch (const exception){
-			cout << "ofxMicroUIRemote :: &&& no internet &&&" << endl;
+	void addUIByNames(string s) {
+		vector <string> names = ofSplitString(s, ",");
+		for (auto & n : names) {
+			addUI(&_u->uis[n]);
 		}
-        
-        setupAll();
-	}
-	
-	void onDraw(ofEventArgs &data) { 
-		// draw(); 
 	}
 
 	void onUpdate(ofEventArgs &data) {
+		if (useSend) {
+			parseSend();
+		}
+		if (useReceive) {
+			parseReceive();
+		}
+	}
+
+	void parseSend() {
 		if (bundle.getMessageCount()) {
 			send.sendBundle(bundle);
 			bundle.clear();
 		}
-		
-		// update();
-
 	}
-	
-	//--------------------------------------------------------------
+
+	void parseReceive() {
+		while(receive.hasWaitingMessages()){
+			ofxOscMessage m;
+			receive.getNextMessage(m);
+//			ofxMicroUI::addAlert(m.getAddress());
+			cout << "receiving message :: " + m.getAddress() << endl;
+
+			string msg = m.getAddress();
+//			ofNotifyEvent(eventMessage, msg);
+			
+			if (oscInfoReceive != NULL) {
+				oscInfoReceive->set(m.getAddress());
+			}
+
+			vector <string> addr = ofSplitString(m.getAddress(), "/");
+			
+			if (m.getAddress() == "/reservedAddress/createFromText") {
+				cout << "YOO" << endl;
+
+				// XAXA todo, ainda fazer 
+				string uiName = "";
+				ofBuffer blob = m.getArgAsBlob(0);
+				ofxMicroUI * _ui;
+				if (u.uis.count(uiName)) {
+					_ui = &u.uis[uiName];
+				} else {
+					u.createFromText("addUI" + uiName);
+					_ui = &u.uis[uiName];
+				}
+				_ui->clear();
+				_ui->initFlow();
+				if (!_ui->hasListeners) {
+					_ui->addListeners();
+				}
+				string lines = blob.getText();
+				_ui->createFromLines(lines);
+				_ui->uiName = uiName;
+				_ui->redrawUI = true;
+//					_ui->autoFit();
+                cout << "UINAME = " << _ui->uiName << endl;
+				cout << "elements size :: " << _ui->elements.size() << endl;
+                cout << lines << endl;
+			}
+			
+			if (addr.size() >= 3) {
+				// cout << addr.size() << endl;
+				string uiName = addr[1];
+				string name = addr[2];
+				// prova de conceito mas eventualmente nao vai funcionar ainda por causa do propagateevent. refazer isso logo em breve
+				
+				if ( _nameUIs.find(uiName) != _nameUIs.end() ) {
+					ofxOscArgType k = m.getArgType(0);
+					_nameUIs[uiName]->_settings->eventFromOsc = true;
+					
+					if (k == OFXOSC_TYPE_FLOAT) {
+						cout << "FLOAT" << endl;
+						_nameUIs[uiName]->set(name, (float) m.getArgAsFloat(0));
+					}
+					else if (k == OFXOSC_TYPE_INT32 || k == OFXOSC_TYPE_INT64) {
+						cout << "INT" << endl;
+						_nameUIs[uiName]->set(name, (int) m.getArgAsInt(0));
+					}
+					else if (k == OFXOSC_TYPE_FALSE) {
+						cout << "BOOL FALSE" << endl;
+						_nameUIs[uiName]->set(name, (bool) false);
+					}
+					else if (k == OFXOSC_TYPE_TRUE) {
+						cout << "BOOL TRUE" << endl;
+						_nameUIs[uiName]->set(name, (bool) true);
+					}
+					else if (k == OFXOSC_TYPE_STRING) {
+						cout << "STRING" << endl;
+						_nameUIs[uiName]->set(name, m.getArgAsString(0));
+					}
+					
+					_nameUIs[uiName]->_settings->eventFromOsc = false;
+					cout << "-------" << endl;
+				}
+			}
+		}
+	}
+
 	void uiEvent(ofxMicroUI::element & e) {
 		cout << e._ui->uiName << " : " << e.name << endl;
 //		cout << sendOnLoadPreset << endl;
@@ -273,14 +259,10 @@ public:
 		if (e._settings->eventFromOsc) {
 			// cout << "EVENT RECEIVED FROM OSC :: ignoring forward " << e.name << endl;
 		} else {
-
-
 			if (sendOnLoadPreset || !e._settings->presetIsLoading) {
 				string address = "/" + e._ui->uiName + "/" + e.name;
-				
 				// transformar em bundle aqui
 		//		cout << "MSG " << address << endl;
-
 				ofxOscMessage m;
 				m.setAddress(address);
 
@@ -330,66 +312,6 @@ public:
 	}
 
 
-	ofxMicroUI * _mirrorUI = NULL;
-	void addMirrorUI(ofxMicroUI * ui) {
-		_mirrorUI = ui;
-		ofAddListener(_nameUIs[ui->uiName]->uiEventMaster, this, &ofxMicroUIRemote::uiEventMirror);
-	}
-	
-	void uiEventMirror(string & e) {
-		cout << "event mirror" << e << endl;
-		// temporario por enquanto. nao sabemos ainda de que UI vem.
-		if (e == "createFromText") {
-			cout << e << endl;	
-			ofxOscMessage m;
-			m.setAddress(reservedAddress + "createFromText");
 
-			ofBuffer blob;
-			blob.append("clear\r");
-			blob.append("uiName\t" + _mirrorUI->uiName +"\r");
 
-			blob.append(_mirrorUI->createdLines);
-			cout << "REMOTE OSC createFromText :: " << endl;
-			cout << _mirrorUI->createdLines << endl;
-			m.addBlobArg(blob);
-			send.sendMessage(m, false);
-		}
-	}
-
-	void uiEventString(string & e) {
-//		cout << "remote event " << e << endl;
-		// temporario por enquanto. nao sabemos ainda de que UI vem.
-		if (e == "createFromText") {
-			cout << e << endl;	
-			ofxOscMessage m;
-			m.setAddress(reservedAddress + "createFromText");
-
-			ofBuffer blob;
-//			if (clear) {
-//				blob.append("uiClear\n");
-//			}
-
-//			if (remoteStyle != "") {
-//				blob.append(remoteStyle);
-//			}
-			
-//			if (ofFile::doesFileExist("uiRemote.txt")) {
-//				blob.append(ofBufferFromFile("uiRemote.txt"));
-//			}
-
-			// this will continue after the event.
-			if (2==3) 
-			{
-
-				blob.append(_nameUIs["master"]->createdLines);
-				cout << "REMOTE OSC createFromText :: " << endl;
-				cout << _nameUIs["master"]->createdLines << endl;
-				m.addBlobArg(blob);
-				send.sendMessage(m, false);
-			}
-
-			
-			//			bundle.addMessage(m);
-		}
-	}
 };
