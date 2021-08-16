74262220f2e8d99d133e29bf02ee6ddd08f200e9
Dimitre <dimitre@users.noreply.github.com>
2021-08-16 16:17:09 -0300

    updates via upgit

--- src/ofxMicroUISoftware.h
+++ src/ofxMicroUISoftware.h
@@ -484,7 +484,7 @@ public:
 		}
 		
 		else if (e.name == "verticalSync") {
-//			cout << "ofxMicroUISoftware :: " << e.name << " :: " << *e.b << endl;
+			cout << "ofxMicroUISoftware :: " << e.name << " :: " << *e.b << endl;
 			ofSetVerticalSync(*e.b);
 		}
 		
